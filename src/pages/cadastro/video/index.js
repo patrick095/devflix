3a1b286bc4b75a7973f20bccaedf8a3ef839696a
Patrick Chaves <patrick095@gmail.com>
2020-08-02 23:51:07 -0300

    add create videos and categories

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import useForm from '../../../hooks/useForm';
 import Button from '../../../components/Button';
 import videosRepository from '../../../respositories/videos';
+import categoriesRepository from '../../../respositories/categories';
 
 function CadastroVideo() {
   const history = useHistory();
+  const [categories, setCategories] = useState([]);
+  const categoriesTitles = categories.map(({ titulo }) => titulo);
   const { handleChange, values } = useForm({
     titulo: '',
     url: '',
     category: '',
   });
+  useEffect(() => {
+    categoriesRepository.getAll()
+      .then((categoriesOnDB) => {
+        setCategories(categoriesOnDB);
+      });
+  }, []);
+
   return (
     <PageDefault>
       <h1>Cadastro de Video</h1>
       <form onSubmit={(event) => {
         event.preventDefault();
+
+        const categoriaId = categories.find((category) => category.titulo === values.category);
         videosRepository.create({
           titulo: values.titulo,
           url: values.url,
-          categoriaId: 1,
+          categoriaId: categoriaId.id,
         })
           .then(() => {
             history.push('/');
@@ -48,6 +60,7 @@ function CadastroVideo() {
           name="category"
           value={values.category}
           onChange={handleChange}
+          suggestions={categoriesTitles}
         />
 
         <Button type="submit">
--- src/respositories/categories.js
+++ src/respositories/categories.js
@@ -2,6 +2,17 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL}/categorias`;
 
+function getAll() {
+  return fetch(URL_CATEGORIES)
+    .then(async (res) => {
+      if (res.ok) {
+        const response = await res.json();
+        return response;
+      }
+      throw new Error('Failed to fetch data :(');
+    });
+}
+
 function getAllCategoriesWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`)
     .then(async (res) => {
@@ -15,4 +26,5 @@ function getAllCategoriesWithVideos() {
 
 export default {
   getAllCategoriesWithVideos,
+  getAll,
 };
